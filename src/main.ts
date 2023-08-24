8fb0b712c63182a2f19b5229589be28171aff4b8
Pratyush-Udhani <pratyush.udhani@gmail.com>
2023-08-24 11:39:47 +0530

    removed models import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { dummyModelCardData as data } from '@/models'; 
 
 type BaseModelType = {
     type: string, 
@@ -22,35 +21,12 @@ export type Text2ImgOutputType = BaseModelType & {
     }
 }
 
-const d = {
-        type: 'txt2img',
-        title: "Kandinsky",
-        description: "Kandinsky inherits best practicies from Dall-E 2 and Latent diffusion, while introducing some new ideas",
-        route: "kandinsky",
-        badge: "kd2",
-        api:'kandinsky2.1-txt2img',
-        defaultOutput: '/ked.png',
-        samplers: ['DDIM',
-            'P-Sampler',
-            // 'PLMS'
-        ],
-        params: {
-            prompt: "tiny isometric city on a tiny floating island, highly detailed, 3d render",
-            negative_prompt: "NONE",
-            samples: 1,
-            scheduler: "DDIM",
-            num_inference_steps: 20,
-            guidance_scale: 7.5,
-            seed: 1024,
-        }
-    }
-
 type Text2ImgInputType = {
 
 }
 
 export const text2img = ({}: Text2ImgInputType) => {
-    console.log(data) 
+    console.log("package works")
 }
 
 
